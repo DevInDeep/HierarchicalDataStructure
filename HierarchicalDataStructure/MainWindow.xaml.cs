639e256e766325747e5a6ffe6769bc8378670b91
vpavlevski <vpavlevski@yahoo.com>
2025-08-09 12:47:58 +0200

    Add project files.

---
+++ HierarchicalDataStructure/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace HierarchicalDataStructure
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e) { }
+    }
+}
---
+++ HierarchicalDataStructure/TestData.cs
@@ -0,0 +1,21 @@
+﻿using HierarchicalDataStructure.Collections;
+
+namespace HierarchicalDataStructure
+{
+    internal static class TestData
+    {
+        internal static TreeNode<string> FetchTestData()
+        {
+            TreeNode<string> person = new TreeNode<string>("Person");
+            person.Children.Add(new TreeNode<string>("First Name"));
+            person.Children.Add(new TreeNode<string>("Last Name"));
+            TreeNode<string> address = new TreeNode<string>("Address");
+            address.Children.Add(new TreeNode<string>("Street"));
+            address.Children.Add(new TreeNode<string>("City"));
+            address.Children.Add(new TreeNode<string>("Postal Code"));
+            address.Children.Add(new TreeNode<string>("Country"));
+            person.Children.Add(address);
+            return person;
+        }
+    }
+}
