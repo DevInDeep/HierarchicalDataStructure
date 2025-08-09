b53ed03f418362caa4ec2bdef4ab49854e8e2918
vpavlevski <vpavlevski@yahoo.com>
2025-08-09 13:00:23 +0200

    Implement Extension Methods

--- HierarchicalDataStructure/Extensions/TreeNodeExtensions.cs
+++ HierarchicalDataStructure/Extensions/TreeNodeExtensions.cs
@@ -1,5 +1,5 @@
-﻿using HierarchicalDataStructure.Collections;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
+using HierarchicalDataStructure.Collections;
 
 namespace HierarchicalDataStructure.Extensions
 {
@@ -7,9 +7,24 @@ namespace HierarchicalDataStructure.Extensions
     {
         internal static IEnumerable<TreeViewItem> Convert<T>(this TreeNode<T> treeNode)
         {
-            TreeViewItem rootItem = new TreeViewItem() { Header = treeNode.Data };
+            TreeViewItem rootTreeViewItem = new TreeViewItem() { Header = treeNode.Data };
+            foreach (TreeNode<T> childTreeNode in treeNode.Children)
+            {
+                TreeViewItem childTreeViewItem = new TreeViewItem() { Header = childTreeNode.Data };
+                rootTreeViewItem.Items.Add(childTreeViewItem);
+                addChildren(childTreeViewItem, childTreeNode);
+            }
+            return [rootTreeViewItem];
+        }
 
-            return new[] { rootItem };
+        private static void addChildren<T>(TreeViewItem treeViewItem, TreeNode<T> treeNode)
+        {
+            foreach (TreeNode<T> childTreeNode in treeNode.Children)
+            {
+                TreeViewItem childTreeViewItem = new TreeViewItem() { Header = childTreeNode.Data };
+                treeViewItem.Items.Add(childTreeViewItem);
+                addChildren(childTreeViewItem, childTreeNode);
+            }
         }
     }
 }
