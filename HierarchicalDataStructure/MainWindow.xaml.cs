d64ed8b9dfcc64a069a0f4703f0f50d88101f6f5
vpavlevski <vpavlevski@yahoo.com>
2025-08-09 12:50:18 +0200

    Design the main window

--- HierarchicalDataStructure/MainWindow.xaml.cs
+++ HierarchicalDataStructure/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace HierarchicalDataStructure
 {
@@ -8,7 +9,7 @@ namespace HierarchicalDataStructure
     public partial class MainWindow : Window
     {
         public MainWindow() => InitializeComponent();
-
         private void MainWindow_Loaded(object sender, RoutedEventArgs e) { }
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e) { }
     }
 }
