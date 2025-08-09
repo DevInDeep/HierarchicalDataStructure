9873f6a0d9a9b29056b0c1f086259a884c0e912b
vpavlevski <vpavlevski@yahoo.com>
2025-08-09 12:57:14 +0200

    Create Extensions class

---
+++ HierarchicalDataStructure/Extensions/TreeNodeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using HierarchicalDataStructure.Collections;
+using System.Windows.Controls;
+
+namespace HierarchicalDataStructure.Extensions
+{
+    internal static class TreeNodeExtensions
+    {
+        internal static IEnumerable<TreeViewItem> Convert<T>(this TreeNode<T> treeNode)
+        {
+            TreeViewItem rootItem = new TreeViewItem() { Header = treeNode.Data };
+
+            return new[] { rootItem };
+        }
+    }
+}
--- HierarchicalDataStructure/MainWindow.xaml.cs
+++ HierarchicalDataStructure/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using HierarchicalDataStructure.Extensions;
 
 namespace HierarchicalDataStructure
 {
@@ -9,7 +10,7 @@ namespace HierarchicalDataStructure
     public partial class MainWindow : Window
     {
         public MainWindow() => InitializeComponent();
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e) { }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e) => treeView.ItemsSource = TestData.FetchTestData().Convert();
         private void txtSearch_TextChanged(object sender, TextChangedEventArgs e) { }
     }
 }
